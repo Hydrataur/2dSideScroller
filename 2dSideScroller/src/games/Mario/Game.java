5da8df9fe6a4c2ac1a8085e813d9f4edf5e5e749
Hydrataur <Hydrataur@DESKTOP-ESUO1R6>
2018-02-22 11:31:49 +0200

    lla

--- 2dSideScroller/src/games/Mario/Game.java
+++ 2dSideScroller/src/games/Mario/Game.java
@@ -7,8 +7,7 @@ import javax.swing.JFrame;
 @SuppressWarnings("serial")
 public class Game extends JFrame{
 
-	//////ai lmao
-	///other
+	//////ai lmao no other
 	
 	public static void main(String[] args) {
 		//Delay creation to allow for processing of GUI first

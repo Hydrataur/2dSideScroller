21e59645957becb29188076e4cda386b073e0539
Hydrataur <Hydrataur@DESKTOP-ESUO1R6>
2018-03-20 10:56:24 +0200

    Minor changes. No bug fixes completed.

--- 2dSideScroller/src/games/Mario/Game.java
+++ 2dSideScroller/src/games/Mario/Game.java
@@ -13,105 +13,114 @@ import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
 @SuppressWarnings("serial")
-public class Game extends JFrame{
-	
+public class Game extends JFrame {
+
 	public static void main(String[] args) {
-		//Delay creation to allow for processing of GUI first
+		// Delay creation to allow for processing of GUI first
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				JFrame frame=new Game();
-				
+				JFrame frame = new Game();
+
 				frame.setContentPane(new JLabel(new ImageIcon("marioImagesNew/MainMenuBG.png")));
-				
+
 				frame.setLayout(null);
-				
-				JButton play=new JButton("PLAY");
-				JButton instructions=new JButton("INSTRUCTIONS");
-				JButton quit=new JButton("QUIT");
-				JButton back=new JButton("BACK");
-				
+
+				JButton play = new JButton("PLAY");
+				JButton instructions = new JButton("INSTRUCTIONS");
+				JButton quit = new JButton("QUIT");
+				JButton back = new JButton("BACK");
+
 				play.setBounds(1350, 500, 500, 200);
 				instructions.setBounds(1350, 750, 500, 200);
 				quit.setBounds(1350, 1000, 500, 200);
 				back.setBounds(200, 1400, 500, 200);
-				
+
 				play.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						play.setVisible(false);
 						instructions.setVisible(false);
 						quit.setVisible(false);
-						
-						MarioBoard mb=new MarioBoard();
+
+						MarioBoard mb = new MarioBoard();
 						mb.setBounds(0, 0, 3200, 1700);
 						frame.add(mb);
 						mb.setVisible(true);
 					}
 				});
-				
-				ImageIcon jumpIcon=new ImageIcon("marioImagesNew/player/JumpingRight.gif");
-				ImageIcon runIcon=new ImageIcon("marioImagesNew/player/RunRight.gif");
-				ImageIcon attackIcon=new ImageIcon("marioImagesNew/player/AttackRight.gif");
-				Image JIcon=jumpIcon.getImage();
-				Image RIcon=runIcon.getImage();
-				Image AIcon=attackIcon.getImage();
-				
-				Image JIcon2=JIcon.getScaledInstance(150, 150, Image.SCALE_FAST);
-				Image RIcon2=RIcon.getScaledInstance(150, 150, Image.SCALE_FAST);
-				Image AIcon2=AIcon.getScaledInstance(150, 150, Image.SCALE_FAST);
-				
-				ImageIcon jumpIcon2=new ImageIcon(JIcon2);
-				ImageIcon runIcon2=new ImageIcon(RIcon2);
-				ImageIcon attackIcon2=new ImageIcon(AIcon2);
-				
-				JLabel jumpGif=new JLabel(jumpIcon2);
-				JLabel runGif=new JLabel(runIcon2);
-				JLabel attackGif=new JLabel(attackIcon2);
-				JTextArea jumpInstruc=new JTextArea("Press the W key or the UP arrow key to jump");
-				JTextArea runInstruc=new JTextArea("Press the LEFT and RIGHT arrow keys or the A and D keys to move");
-				JTextArea attackInstruc=new JTextArea("Press the SPACE key to attack");
-				
+
+				ImageIcon jumpIcon = new ImageIcon("marioImagesNew/player/JumpingRight.gif");
+				ImageIcon runIcon = new ImageIcon("marioImagesNew/player/RunRight.gif");
+				ImageIcon attackIcon = new ImageIcon("marioImagesNew/player/AttackRight.gif");
+				Image JIcon = jumpIcon.getImage();
+				Image RIcon = runIcon.getImage();
+				Image AIcon = attackIcon.getImage();
+
+				Image JIcon2 = JIcon.getScaledInstance(150, 150, Image.SCALE_FAST);
+				Image RIcon2 = RIcon.getScaledInstance(150, 150, Image.SCALE_FAST);
+				Image AIcon2 = AIcon.getScaledInstance(150, 150, Image.SCALE_FAST);
+
+				ImageIcon jumpIcon2 = new ImageIcon(JIcon2);
+				ImageIcon runIcon2 = new ImageIcon(RIcon2);
+				ImageIcon attackIcon2 = new ImageIcon(AIcon2);
+
+				JLabel jumpGif = new JLabel(jumpIcon2);
+				JLabel runGif = new JLabel(runIcon2);
+				JLabel attackGif = new JLabel(attackIcon2);
+				JTextArea jumpInstruc = new JTextArea("Press the W key or the UP arrow key to jump");
+				JTextArea runInstruc = new JTextArea("Press the LEFT and RIGHT arrow keys or the A and D keys to move",
+						500, 500);
+				JTextArea attackInstruc = new JTextArea("Press the SPACE key to attack");
+
 				jumpInstruc.setFont(new Font("Impact", Font.BOLD, 30));
 				runInstruc.setFont(new Font("Impact", Font.BOLD, 30));
 				attackInstruc.setFont(new Font("Impact", Font.BOLD, 30));
+
+				jumpInstruc.setOpaque(false);
+				jumpInstruc.setEditable(false);
+				runInstruc.setOpaque(false);
+				runInstruc.setEditable(false);
+				attackInstruc.setOpaque(false);
+				attackInstruc.setEditable(false);
 				
-				jumpGif.setBounds(1500, 500, 200, 200);
-				runGif.setBounds(1500, 700, 200, 200);
-				attackGif.setBounds(1500, 900, 200, 200);
-				jumpInstruc.setBounds(1700, 500, 610, 100);
-				runInstruc.setBounds(1700, 700, 900, 100);
-				attackInstruc.setBounds(1700, 900, 430, 100);
 				
-				
+
+				jumpGif.setBounds(1300, 500, 200, 200);
+				runGif.setBounds(1300, 700, 200, 200);
+				attackGif.setBounds(1300, 900, 200, 200);
+				jumpInstruc.setBounds(1500, 550, 610, 100);
+				runInstruc.setBounds(1500, 750, 900, 100);
+				attackInstruc.setBounds(1500, 950, 430, 100);
+
 				instructions.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						play.setVisible(false);
 						instructions.setVisible(false);
 						quit.setVisible(false);
-						
+
 						frame.add(jumpGif);
 						frame.add(runGif);
 						frame.add(attackGif);
 						frame.add(jumpInstruc);
 						frame.add(runInstruc);
 						frame.add(attackInstruc);
-						
+
 						jumpGif.setVisible(true);
 						runGif.setVisible(true);
 						attackGif.setVisible(true);
 						jumpInstruc.setVisible(true);
 						runInstruc.setVisible(true);
 						attackInstruc.setVisible(true);
-						
+
 						back.setVisible(true);
 					}
 				});
-				
+
 				quit.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						frame.dispose();
 					}
 				});
-				
+
 				back.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						jumpGif.setVisible(false);
@@ -120,38 +129,38 @@ public class Game extends JFrame{
 						jumpInstruc.setVisible(false);
 						runInstruc.setVisible(false);
 						attackInstruc.setVisible(false);
-						
+
 						back.setVisible(false);
 						play.setVisible(true);
 						instructions.setVisible(true);
 						quit.setVisible(true);
 					}
 				});
-				
+
 				play.setFont(new Font("Impact", Font.BOLD, 50));
 				instructions.setFont(new Font("Impact", Font.BOLD, 50));
 				quit.setFont(new Font("Impact", Font.BOLD, 50));
 				back.setFont(new Font("Impact", Font.BOLD, 50));
-				
+
 				frame.add(play);
 				frame.add(instructions);
 				frame.add(quit);
 				frame.add(back);
-				
+
 				play.setVisible(true);
 				instructions.setVisible(true);
 				quit.setVisible(true);
 				back.setVisible(false);
-				
+
 				frame.setBounds(0, 0, 3200, 1700);
-				
+
 				frame.setResizable(false);
-				
+
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				
+
 				frame.setVisible(true);
 			}
 		});
 	}
-	
+
 }
